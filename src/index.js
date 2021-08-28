882ed5916909695904a96c06d2d711d60e912268
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-08-28 13:37:57 -0300

    Create: Type - User (index.js)

--- src/index.js
+++ src/index.js
@@ -6,17 +6,31 @@ const { ApolloServer, gql } = require('apollo-server');
 // Mutation -> (POST || PUT || PATH || DELETE)
 
 const typeDefs = gql`
+
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
   type Query {
     hello: String
+    users: [User!]!
   }
 `;
 
 const resolvers = {
   Query: {
-
+    hello: () => "Hello World"
+    
   }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then( ({url}) => console.log(`Server Started on ${url}`))
+
+// Start gql server and open playground
+//
+// gql node src/index.js
